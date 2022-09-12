96de54355072c07ccac00362372fa91cab0fafe2
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-12 15:37:12 +0200

    Removed abstract from ERC721COMMON contract and copied over constructor from ERC721.sol

--- contracts/token/ERC721/ERC721FCOMMON.sol
+++ contracts/token/ERC721/ERC721FCOMMON.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9 <0.9.0;
 import "./ERC721F.sol";
 import "./extensions/ERC721Payable.sol";
 
-abstract contract ERC721FCOMMON is ERC721F, ERC721Payable {
-
+contract ERC721FCOMMON is ERC721F, ERC721Payable {
+    constructor(string memory name_, string memory symbol_) ERC721F(name_, symbol_) {
+    }
 }
