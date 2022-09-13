8c4dd2595a5fba6ce4c296e150c041d4bb0c3e14
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-13 12:54:04 +0200

    Testing basic contract functions/deployment states

--- test/token/ERC721/ERC721F.test.js
+++ test/token/ERC721/ERC721F.test.js
@@ -13,4 +13,24 @@ describe("Token contract", function () {
 
         return { Token, hardhatToken, owner, addr1 };
     }
+    
+    describe("Deployment", function () {
+        it("Should set the right owner", async function () {
+            const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
+
+            expect(await hardhatToken.owner()).to.equal(owner.address);
+        });
+        
+        it("Shouldn't have any tokens in the owners wallet", async function() {
+            const { hardhatToken, owner } = await loadFixture(deployTokenFixture);
+
+            expect(await hardhatToken.walletOfOwner(owner.address)).to.eql([]);
+        });
+
+        it("Contains an empty total supply", async function() {
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            expect(await hardhatToken.totalSupply()).to.equal(0);
+        });
+    });
 });
