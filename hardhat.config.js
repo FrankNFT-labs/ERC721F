7e670a9a1a2743a8b0e7cdf62c4235740fc16c65
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-13 12:51:28 +0200

    Added pathvariables to hardhat.config.js to customize file pathlocations

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  paths: {
+    sources: "./examples",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
