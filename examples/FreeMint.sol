864ce2218cc7c0e560f5158ed419e6d91c8a9634
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-13 11:25:31 +0200

    Setup examples folder in root folder and copied over FreeMint.sol in it

---
+++ examples/FreeMint.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.9 <0.9.0;
+
+import "https://github.com/FrankNFT-labs/ERC721F/blob/v4.7.0/contracts/token/ERC721/ERC721F.sol";
+
+contract FreeMint is ERC721F {
+
+    uint256 public constant MAX_TOKENS = 10000;
+    uint public constant MAX_PURCHASE = 31; 
+    bool public saleIsActive;
+
+    
+    constructor() ERC721F("FreeMint", "Free") {
+        setBaseTokenURI("ipfs://QmVy7VQUFtTQawBsp4tbJPp9MgbTKS4L7WSDpZEdZUzsiD/"); 
+    }
+
+    function flipSaleState() external onlyOwner {
+        saleIsActive = !saleIsActive;
+    }
+
+    /**
+     * Mint your tokens here.
+     */
+    function mint(uint256 numberOfTokens) external {
+        require(msg.sender==tx.origin,"No Contracts allowed.");
+        require(saleIsActive,"Sale NOT active yet");
+        require(numberOfTokens != 0, "numberOfNfts cannot be 0");
+        require(numberOfTokens < MAX_PURCHASE, "Can only mint 30 tokens at a time");
+        uint256 supply = totalSupply();
+        require(supply + numberOfTokens <= MAX_TOKENS, "Purchase would exceed max supply of Tokens");
+        for(uint256 i; i < numberOfTokens;){
+            _mint( msg.sender, supply + i ); // no need to use safeMint as we don't allow contracts.
+            unchecked{ i++;}
+        }
+    }
+}
