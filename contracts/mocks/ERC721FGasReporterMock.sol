055af0d82351957f9d772a7e33b959e338100876
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-13 16:12:28 +0200

    Setup main mint method and subsequent methods which call the main method with increasing parameter values

--- contracts/mocks/ERC721FGasReporterMock.sol
+++ contracts/mocks/ERC721FGasReporterMock.sol
@@ -12,4 +12,36 @@ import '../token/ERC721/ERC721F.sol';
 contract ERC721FGasReporterMock is ERC721F {
     constructor(string memory name_, string memory symbol_) ERC721F(name_, symbol_) {
     }
+
+    /**
+     * @notice Mints a single token
+     */
+    mintOne(address to) public {
+        mint(to, 1)
+    }
+
+    /**
+     * @notice Mints ten tokens
+     */
+    mintTen(address to) public {
+        mint(to, 10);
+    }
+
+    /**
+     * @notice Mints a hundred tokens
+     */
+    mintHundred(address to) public {
+        mint(to, 100);
+    }
+
+    /**
+     * Mints any number of tokens and transfers them to `to`
+     */
+    function mint(address to, uint256 numberOfTokens) external {
+        uint256 supply = totalSupply();
+        for (uint256 i = 0; i < numberOfTokens) {
+            _mint(msg.sender, supply + i);
+            unchecked {i++}
+        }
+    }
 }
