dc025224e119742c3390792bf5f0fdb15447c4ba
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-21 16:21:01 +0200

    Fix deployment testing, testing flip functions and moved payment testing of pre-sale to pre-sale

--- test/examples/MerkleRoot.test.js
+++ test/examples/MerkleRoot.test.js
@@ -25,19 +25,82 @@ describe.only("Token Contract", function () {
         it("Should have defined the root", async function () {
             const { hardhatToken } = await loadFixture(deployTokenFixture);
 
-            expect(await hardhatToken.root).to.not.be.null;
-            expect(await hardhatToken.root).to.not.be.undefined;
+            expect(await hardhatToken.root()).to.not.be.null;
+            expect(await hardhatToken.root()).to.not.be.undefined;
         });
 
-        it("Shouldn't have defined isActive yet", async function () {
+        it("Should have set saleIsActive to false", async function () {
             const { hardhatToken } = await loadFixture(deployTokenFixture);
 
-            expect(await hardhatToken.isActive).to.be.undefined;
+            expect(await hardhatToken.saleIsActive()).to.be.false;
+        });
+
+        it("Should have set isPreSaleActive to false", async function() {
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            expect(await hardhatToken.preSaleIsActive()).to.be.false;
+        });
+    });
+
+    describe("SaleFlipping", function() {
+        it("Should flip saleIsActive to true", async function() {
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            await hardhatToken.flipSaleState();
+
+            expect(await hardhatToken.saleIsActive()).to.be.true;
+        });
+
+        it("Should flip saleIsActive to false if flipping twice", async function() {
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            await hardhatToken.flipSaleState();
+            await hardhatToken.flipSaleState();
+
+            expect(await hardhatToken.saleIsActive()).to.be.false;
+        });
+    });
+
+    describe("PreSaleFlipping", function() {
+        it("Should flip preSaleIsActive to true", async function() {
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            await hardhatToken.flipPreSaleState();
+
+            expect(await hardhatToken.preSaleIsActive()).to.be.true;
+        });
+
+        it("Should flip preSaleIsActive to false when flipping twice", async function() {
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            await hardhatToken.flipPreSaleState();
+            await hardhatToken.flipPreSaleState();
+
+            expect(await hardhatToken.preSaleIsActive()).to.be.false;
         });
     });
 
     describe("Pre-Sale Minting", function () {
-        it("Should allow minting by whitelisted accounts during inactive sale period", async function () {
+        it("Shouldn't allow minting by whitelisted accounts during inactive pre-sale period", async function() {
+            const { hardhatToken, addr1 } = await loadFixture(deployTokenFixture);
+
+            const merkleProof = createProof(addr1.address);
+
+            await expect(hardhatToken.connect(addr1).mintPreSale(1, merkleProof)).to.be.revertedWith("PreSale is not active yet");
+        });
+
+        it ("Shouldn't allow minting by whitelisted accounts which don't send enough funds", async function() {
+            const { hardhatToken , addr1 } = await loadFixture(deployTokenFixture);
+
+            const merkleProof = createProof(addr1.address);
+            await hardhatToken.flipPreSaleState();
+
+            await expect(hardhatToken.connect(addr1).mintPreSale(1, merkleProof, {
+                value: ethers.utils.parseEther("0.2")
+            })).to.be.revertedWith("Insufficient funds");
+        });
+
+        it("Should allow minting by whitelisted accounts during active pre-sale period", async function () {
             const { hardhatToken, addr1 } = await loadFixture(deployTokenFixture);
 
             const merkleProof = createProof(addr1.address);
