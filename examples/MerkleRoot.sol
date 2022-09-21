4adc5acdde6af14b05d406bd28d961099d028198
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-21 13:54:06 +0200

    Added single minting functionality which uses MerkleProof to validate pre-sale accounts

--- examples/MerkleRoot.sol
+++ examples/MerkleRoot.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.9 <0.9.0;
 
 import "../contracts/token/ERC721/ERC721F.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 /**
  * @title MerkleRoot
@@ -10,6 +11,27 @@ import "../contracts/token/ERC721/ERC721F.sol";
  */
 contract MerkleRoot is ERC721F {
 
-    constructor() ERC721F("MerkleRoot Pre-Sale", "Merkle") {
+    uint256 public constant MAX_TOKENS = 10000;
+    uint public constant MAX_PURCHASE = 31; 
+    bool public saleIsActive;
+    bytes32 public root;
+
+    constructor(bytes32 _root) ERC721F("MerkleRoot Pre-Sale", "Merkle") {
+        root = _root;
+    }
+
+    function mint(bytes32[] calldata merkleProof) external {
+        require(msg.sender == tx.origin, "No Contracts allowed.");
+        uint256 supply = totalSupply();
+        require(supply <= MAX_TOKENS, "Purchase would exceed max supply of Tokens");
+        if (!saleIsActive) {
+            require(checkValidity(merkleProof), "Invalid Merkle Proof");
+        }
+        _mint(msg.sender, supply + 1);
+    }
+
+    function checkValidity(bytes32[] calldata merkleProof) public view returns (bool) {
+        bytes32 leafToCheck = keccak256(abi.encodePacked(msg.sender));
+        return MerkleProof.verify(merkleProof, root, leafToCheck);
     }
 }
