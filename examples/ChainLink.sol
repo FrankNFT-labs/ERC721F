bdfb4f56ba26a0acb77c32fc04d67b25924d467c
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-28 14:18:56 +0200

    Made callbackGasLimit, requestConfirmations and numWords constant values since they won't change during the contract lifetime

--- examples/ChainLink.sol
+++ examples/ChainLink.sol
@@ -23,13 +23,13 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
     bytes32 keyHash =
         0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
 
-    uint32 callbackGasLimit = 100000;
+    uint32 constant callbackGasLimit = 100000;
 
     // The default is 3, but you can set this higher.
-    uint16 requestConfirmations = 3;
+    uint16 constant requestConfirmations = 3;
 
     // Single number is required for setting the startingIndex
-    uint32 numWords = 1;
+    uint32 constant numWords = 1;
 
     uint256 public constant MAX_TOKENS = 10000;
     uint public constant MAX_PURCHASE = 31;
