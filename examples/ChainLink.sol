26f26c3556091fd2828bb7608bc4e6f3a589a42a
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-28 14:01:32 +0200

    Addition of documentation and overriding of walletOfOwner function to prevent "Non-existent tokenId" error

--- examples/ChainLink.sol
+++ examples/ChainLink.sol
@@ -22,10 +22,10 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
     bytes32 keyHash =
         0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
 
-    uint32 callbackGasLimit = 100000; // Add set method
+    uint32 callbackGasLimit = 100000;
 
     // The default is 3, but you can set this higher.
-    uint16 requestConfirmations = 3; // Add set method
+    uint16 requestConfirmations = 3;
 
     // Single number is required for setting the startingIndex
     uint32 numWords = 1;
@@ -35,6 +35,7 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
     uint256 public startingIndex;
     bool public saleIsActive;
 
+    // Event utilised in hardhat testing to retrieve requestId from ChainLink request 
     event RequestedRandomness(uint256 requestId);
 
     constructor(uint64 _subscriptionId, address _vrfCoordinator)
@@ -45,6 +46,10 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
         subscriptionId = _subscriptionId;
     }
 
+    /**
+     * @notice Flips saleIsActive from `true` to `false` and `false` to `true`
+     * @dev When activing the sale it's required to have set the startingIndex
+     */
     function flipSaleState() external onlyOwner {
         if (!saleIsActive)
             require(
@@ -54,6 +59,11 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
         saleIsActive = !saleIsActive;
     }
 
+    /**
+     * @notice Makes a request for a singular random number 
+     * @dev Emits RequestRandomness with `requestId` as parameter for retrieval within `/tests/examples/ChainLink.test.js`
+     * @dev Assumes that subscriptionId is set and funded (also requires contractaddress to be assigned as consumer)
+     */
     function setRandomStartingIndex() external onlyOwner {
         require(startingIndex == 0, "startingIndex already set");
 
@@ -68,6 +78,10 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
         emit RequestedRandomness(requestId);
     }
 
+    /**
+     * @notice Mint your tokens here.
+     * @dev Uses randomely set startingIndex as first mint, subsequent mints will loop around until startingIndex - 1 has been reached  
+     */
     function mint(uint32 numberOfTokens) external {
         require(msg.sender == tx.origin, "No Contracts allowed.");
         require(saleIsActive, "Sale NOT active yet");
@@ -91,10 +105,41 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
         }
     }
 
+    /**
+     * @notice Assigns first index of `randomWords` to `startingIndex`, altered to be between 1 and `MAX_TOKENS`
+     * @dev Function which receives response of setRandomIndex
+     */
     function fulfillRandomWords(uint256, uint256[] memory randomWords)
         internal
         override
     {
         startingIndex = (randomWords[0] % MAX_TOKENS) + 1;
+    }
+
+    /**
+     * @notice Returns all tokenIds owned by `_owner`
+     * @dev Override of walletOfOwner located in ERC721F, using startingIndex as startpoint of counting instead of _startTokenId
+     * @return Array of all tokenIds owned by `_owner` starting from startingIndex
+     */
+    function walletOfOwner(address _owner) external view virtual override returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+        uint256 currentTokenId = startingIndex;
+        uint256 ownedTokenIndex = 0;
+        uint256 totalTokensChecked = 0;
+
+        while (ownedTokenIndex < ownerTokenCount && totalTokensChecked < MAX_TOKENS) {
+            if (ownerOf(currentTokenId) == _owner) {
+                ownedTokenIds[ownedTokenIndex] = currentTokenId;
+                unchecked {
+                    ownedTokenIndex++;
+                }
+            }
+            unchecked {
+                currentTokenId = (currentTokenId + 1) % MAX_TOKENS;
+                totalTokensChecked++;
+            }
+        }
+        return ownedTokenIds;
     }
 }
