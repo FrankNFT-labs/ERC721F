9b287e4e00656704fb9b214efe21e4fd4d8661a3
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-28 12:20:48 +0200

    Changed implementation of VRF to only assign startingIndex instead of generating a random number after every mintrequest

--- examples/ChainLink.sol
+++ examples/ChainLink.sol
@@ -2,11 +2,8 @@
 pragma solidity ^0.8.9 <0.9.0;
 
 import "../contracts/token/ERC721/ERC721F.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 
 /**
  * @title ChainLink
@@ -19,6 +16,9 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
 
     // Your subscription ID.
     uint64 subscriptionId;
+
+    // Respective values of goerli test network, find other networks keyHash/vrfCoordinator at https://docs.chain.link/docs/vrf/v2/subscription/supported-networks/#configurations
+    address vrfCoordinator = 0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
     bytes32 keyHash =
         0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
 
@@ -27,69 +27,68 @@ contract ChainLink is ERC721F, VRFConsumerBaseV2 {
     // The default is 3, but you can set this higher.
     uint16 requestConfirmations = 3; // Add set method
 
-    // For this example, retrieve 2 random values in one request.
-    // Cannot exceed VRFCoordinatorV2.MAX_NUM_WORDS.
-    uint32 numWords = 1; // Add set method
+    // Single number is required for setting the startingIndex
+    uint32 numWords = 1;
 
-    uint256[] public randomWords;
     uint256 public constant MAX_TOKENS = 10000;
     uint public constant MAX_PURCHASE = 31;
+    uint256 public startingIndex;
     bool public saleIsActive;
 
-    mapping(uint256 => address) requestToSender;
-
-    event ReceivedRandomness(uint256 reqId, uint256[] randomWords);
-    event RequestedRandomness(uint256 reqId, address invoker);
-
-    constructor(uint64 _subscriptionId, address _vrfCoordinator)
-        VRFConsumerBaseV2(_vrfCoordinator)
+    constructor(uint64 _subscriptionId)
+        VRFConsumerBaseV2(vrfCoordinator)
         ERC721F("ChainLink", "Chain")
     {
-        COORDINATOR = VRFCoordinatorV2Interface(_vrfCoordinator);
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
         subscriptionId = _subscriptionId;
     }
 
     function flipSaleState() external onlyOwner {
+        if (!saleIsActive)
+            require(
+                startingIndex != 0,
+                "startingIndex must be set before sale can begin"
+            );
         saleIsActive = !saleIsActive;
     }
 
-    function mint(uint32 numberOfTokens) public returns (uint256 requestId) {
-        require(saleIsActive, "Sale NOT active yet");
-        require(numberOfTokens != 0, "numberOfNfts cannot be 0");
-        require(
-            numberOfTokens < MAX_PURCHASE,
-            "Can only mint 30 tokens at a time"
-        );
-        require(
-            totalSupply() + 1 <= MAX_TOKENS,
-            "Purchase would exceed max supply of Tokens"
-        );
-
-        require(
-            numberOfTokens <= 500,
-            "Can't request more words than internal limit set by ChainLink"
-        );
-        // Will revert if subscription is not set and funded.
-        requestId = COORDINATOR.requestRandomWords(
+    function setRandomStartingIndex() external onlyOwner {
+        require(startingIndex == 0, "startingIndex already set");
+        COORDINATOR.requestRandomWords(
             keyHash,
             subscriptionId,
             requestConfirmations,
             callbackGasLimit,
-            numberOfTokens
+            numWords
         );
-
-        requestToSender[requestId] = msg.sender;
-        emit RequestedRandomness(requestId, msg.sender);
     }
 
-    function fulfillRandomWords(
-        uint256 requestId,
-        uint256[] memory _randomWords
-    ) internal override {
-        randomWords = _randomWords;
+    function mint(uint32 numberOfTokens) external {
+        require(saleIsActive, "Sale NOT active yet");
+        require(numberOfTokens != 0, "numberOfTokens cannot be 0");
+        require(
+            numberOfTokens < MAX_PURCHASE,
+            "Can only mint 30 tokens at a time"
+        );
 
-        //address sender = requestToSender[requestId];
-        //_mint(sender, _randomWords[0]);
-        emit ReceivedRandomness(requestId, randomWords);
+        uint256 supply = totalSupply();
+        require(
+            supply + numberOfTokens <= MAX_TOKENS,
+            "Purchase would exceed max supply of Tokens"
+        );
+
+        for (uint256 i; i < numberOfTokens; ) {
+            _mint(msg.sender, (startingIndex + supply + i) % MAX_TOKENS); // no need to use safeMint as we don't allow contracts.
+            unchecked {
+                i++;
+            }
+        }
+    }
+
+    function fulfillRandomWords(uint256, uint256[] memory randomWords)
+        internal
+        override
+    {
+        startingIndex = (randomWords[0] % MAX_TOKENS) + 1;
     }
 }
