27782387b6b9ded9aef16e2a663bfae9d6117265
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-30 16:13:12 +0200

    Overwrote tokenOfOwnerByIndex

--- contracts/token/ERC721/extensions/ERC721FEnumerable.sol
+++ contracts/token/ERC721/extensions/ERC721FEnumerable.sol
@@ -10,7 +10,23 @@ abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
+        require(index < balanceOf(owner));
+        uint256 totalMinted = totalSupply();
+        uint256 currentTokenId = _startTokenId();
+        uint256 ownedTokenIndex = 0;
 
+        while (ownedTokenIndex <= index && currentTokenId < totalMinted) {
+            if (ownerOf(currentTokenId) == owner) {
+                if (currentTokenId == index) return currentTokenId;
+                unchecked {
+                    ownedTokenIndex++;
+                }
+            }
+            unchecked {
+                currentTokenId++;
+            }
+        }  
+        revert();
     }
 
     function tokenByIndex(uint256 index) external view returns(uint256) {
