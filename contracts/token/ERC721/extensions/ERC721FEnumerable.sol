79ed63a2bd282062b397980cb0c90917058c1e82
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-30 16:05:33 +0200

    Implementing ERC721F to allow usage totalSupply, overwrote totalSupply due to conflict of both implementation containing totalSupply

--- contracts/token/ERC721/extensions/ERC721FEnumerable.sol
+++ contracts/token/ERC721/extensions/ERC721FEnumerable.sol
@@ -4,12 +4,16 @@ pragma solidity ^0.8.9 <0.9.0;
 import "../ERC721F.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
-abstract contract ERC721Enumerable is IERC721Enumerable {
+abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
+    function totalSupply() public view override(ERC721F, IERC721Enumerable) returns (uint256) {
+        return ERC721F.totalSupply();
+    }
+
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
-        
+
     }
 
     function tokenByIndex(uint256 index) external view returns(uint256) {
-        
+
     }
 }
