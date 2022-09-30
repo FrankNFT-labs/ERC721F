c9a7295cca6d866a05cd9267cb0585bcdb02a676
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-30 16:33:36 +0200

    Override tokenByIndex and clarified require of tokenOfOwnerByIndex

--- contracts/token/ERC721/extensions/ERC721FEnumerable.sol
+++ contracts/token/ERC721/extensions/ERC721FEnumerable.sol
@@ -5,13 +5,14 @@ import "../ERC721F.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
+
     function totalSupply() public view override(ERC721F, IERC721Enumerable) returns (uint256) {
         return ERC721F.totalSupply();
     }
 
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
-        require(index < balanceOf(owner));
-        uint256 totalMinted = totalSupply();
+        require(index < balanceOf(owner), "Index out of bounds for owned tokens");
+        uint256 totalMinted = ERC721FEnumerable.totalSupply();
         uint256 currentTokenId = _startTokenId();
         uint256 ownedTokenIndex = 0;
 
@@ -30,6 +31,16 @@ abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
     }
 
     function tokenByIndex(uint256 index) external view returns(uint256) {
+        uint256 totalMinted = ERC721FEnumerable.totalSupply();
+        require(index < totalMinted, "Index out of bounds for total minted tokens");
+        uint256 currentTokenIndex = _startTokenId();
 
+        while(currentTokenIndex < totalMinted) {
+            if(currentTokenIndex == index) return currentTokenIndex;
+            unchecked {
+                currentTokenIndex++;
+            }
+        }
+        revert();
     }
 }
