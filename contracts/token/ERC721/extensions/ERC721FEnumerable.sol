67c522693db1a9b58e3cdd06f85756d1da511168
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-30 16:44:38 +0200

    Code format

--- contracts/token/ERC721/extensions/ERC721FEnumerable.sol
+++ contracts/token/ERC721/extensions/ERC721FEnumerable.sol
@@ -5,12 +5,16 @@ import "../ERC721F.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
-
     /**
      * @dev See {IERC721Enumerable-totalSupply}
      * Returns total amount of tokens stored in the contract
      */
-    function totalSupply() public view override(ERC721F, IERC721Enumerable) returns (uint256) {
+    function totalSupply()
+        public
+        view
+        override(ERC721F, IERC721Enumerable)
+        returns (uint256)
+    {
         return ERC721F.totalSupply();
     }
 
@@ -18,8 +22,15 @@ abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
      * @dev See {IERC721Enumerable-tokenOfOwnerByIndex}
      * Returns token ID owned by `owner` at a given `index` of its token list
      */
-    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
-        require(index < balanceOf(owner), "Index out of bounds for owned tokens");
+    function tokenOfOwnerByIndex(address owner, uint256 index)
+        external
+        view
+        returns (uint256)
+    {
+        require(
+            index < balanceOf(owner),
+            "Index out of bounds for owned tokens"
+        );
         uint256 totalMinted = ERC721FEnumerable.totalSupply();
         uint256 currentTokenId = _startTokenId();
         uint256 ownedTokenIndex = 0;
@@ -34,21 +45,24 @@ abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
             unchecked {
                 currentTokenId++;
             }
-        }  
+        }
         revert();
     }
 
     /**
      * @dev See {IERC721Enumerable-tokenByIndex}
-     * Returns a token ID at a given `index` of all the tokens stored by the contract 
+     * Returns a token ID at a given `index` of all the tokens stored by the contract
      */
-    function tokenByIndex(uint256 index) external view returns(uint256) {
+    function tokenByIndex(uint256 index) external view returns (uint256) {
         uint256 totalMinted = ERC721FEnumerable.totalSupply();
-        require(index < totalMinted, "Index out of bounds for total minted tokens");
+        require(
+            index < totalMinted,
+            "Index out of bounds for total minted tokens"
+        );
         uint256 currentTokenIndex = _startTokenId();
 
-        while(currentTokenIndex < totalMinted) {
-            if(currentTokenIndex == index) return currentTokenIndex;
+        while (currentTokenIndex < totalMinted) {
+            if (currentTokenIndex == index) return currentTokenIndex;
             unchecked {
                 currentTokenIndex++;
             }
