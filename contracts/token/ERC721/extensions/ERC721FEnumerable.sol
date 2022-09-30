be58ea29e2792f875cc1c07c1e75cc7d1b29fc9c
HansRobrecht <hans.robrecht@student.odisee.be>
2022-09-30 16:44:28 +0200

    Added documentation

--- contracts/token/ERC721/extensions/ERC721FEnumerable.sol
+++ contracts/token/ERC721/extensions/ERC721FEnumerable.sol
@@ -6,10 +6,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
 
+    /**
+     * @dev See {IERC721Enumerable-totalSupply}
+     * Returns total amount of tokens stored in the contract
+     */
     function totalSupply() public view override(ERC721F, IERC721Enumerable) returns (uint256) {
         return ERC721F.totalSupply();
     }
 
+    /**
+     * @dev See {IERC721Enumerable-tokenOfOwnerByIndex}
+     * Returns token ID owned by `owner` at a given `index` of its token list
+     */
     function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
         require(index < balanceOf(owner), "Index out of bounds for owned tokens");
         uint256 totalMinted = ERC721FEnumerable.totalSupply();
@@ -30,6 +38,10 @@ abstract contract ERC721FEnumerable is ERC721F, IERC721Enumerable {
         revert();
     }
 
+    /**
+     * @dev See {IERC721Enumerable-tokenByIndex}
+     * Returns a token ID at a given `index` of all the tokens stored by the contract 
+     */
     function tokenByIndex(uint256 index) external view returns(uint256) {
         uint256 totalMinted = ERC721FEnumerable.totalSupply();
         require(index < totalMinted, "Index out of bounds for total minted tokens");
