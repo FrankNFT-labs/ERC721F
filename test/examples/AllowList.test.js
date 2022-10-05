cf8ab2f4b1749fcd44e778de982f87c0d0828ec8
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-05 13:24:52 +0200

    Setup testing environment of AllowList

--- test/examples/AllowList.test.js
+++ test/examples/AllowList.test.js
@@ -262,4 +262,19 @@ describe("AllowList", function () {
             await expect(hardhatToken.withdraw()).to.be.revertedWith("Insufficient balance");
         })
     });
+
+    context.only("AllowList", function() {
+        let token;
+        let whitelistedAddress;
+        let nonWhitelistedAddress;
+        let secondNonWhitelistedAddress;
+
+        beforeEach(async () => {
+            const { hardhatToken, addr1, addr6, addr7 } = await loadFixture(deployTokenFixture);
+            token = hardhatToken;
+            whitelistedAddress = addr1;
+            nonWhitelistedAddress = addr6;
+            secondNonWhitelistedAddress = addr7
+        });
+    })
 });
