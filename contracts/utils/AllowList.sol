3a5adf8ea19b7807a6dc20e6705ee8e0e3e674a6
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-05 09:32:18 +0200

    Removed decryptingcode and implemented allowlist functionality through a mapping

--- contracts/utils/AllowList.sol
+++ contracts/utils/AllowList.sol
@@ -2,13 +2,35 @@
 pragma solidity ^0.8.9 <0.9.0;
 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-library AllowList {
-    function recoverSigner(bytes32 hash, bytes memory signature) public pure returns(address) {
-        bytes32 messageDigest = keccak256(abi.encodePacked(
-            "\x19Ethereum Signed Message:\n32",
-            hash
-        ));
-        return ECDSA.recover(messageDigest, signature);
+abstract contract AllowList is Ownable {
+    mapping(address => bool) allowList;
+
+    modifier onlyAllowList() {
+        require(isAllowList(msg.sender), "Address is not within allowList");
+        _;
+    }
+
+    function allowAddress(address _address) public onlyOwner {
+        allowList[_address] = true;
+    }
+
+    function allowAddresses(address[] calldata _addresses) external onlyOwner {
+        uint length = _addresses.length;
+        for (uint i; i < length; ) {
+            allowAddress(_addresses[i]);
+            unchecked {
+                i++;
+            }
+        }
+    }
+
+    function disallowAddress(address _address) public onlyOwner {
+        allowList[_address] = false;
+    }
+
+    function isAllowList(address _address) public view returns (bool) {
+        return allowList[_address];
     }
 }
