de9b517aea850535ddcd2ac58d73b2f859560401
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-05 09:35:53 +0200

    Removed unused import

--- contracts/utils/AllowList.sol
+++ contracts/utils/AllowList.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9 <0.9.0;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract AllowList is Ownable {
