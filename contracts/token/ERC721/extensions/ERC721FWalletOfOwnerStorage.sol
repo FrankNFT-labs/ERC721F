a5dded42f774c641a9963316f34b2c807f35422c
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-11 11:12:36 +0200

    Added basic documentation

--- contracts/token/ERC721/extensions/ERC721FWalletOfOwnerStorage.sol
+++ contracts/token/ERC721/extensions/ERC721FWalletOfOwnerStorage.sol
@@ -3,15 +3,27 @@ pragma solidity ^0.8.9 <0.9.0;
 
 import "../ERC721F.sol";
 
+/**
+ * @title ERC721FWalletOfOwnerStrorage
+ * 
+ * @dev Extension of ERC721F, which overrides default walletOfOwner functionality to utilise a mapping instead of looping through the token collection
+ */
 abstract contract ERC721FWalletOfOwnerStorage is ERC721F {
     mapping(address => uint256[]) _walletOfOwner;
 
+    /**
+     * @dev walletOfOwner
+     * @return tokens id owned by `_owner` 
+     */
     function walletOfOwner(address _owner) external view virtual override returns (uint256[] memory) {
         return _walletOfOwner[_owner];
     }
 
+    /**
+     * @dev Pushes `tokenId` to _walletOfOwner of `to`
+     */
     function _mint(address to, uint256 tokenId) internal virtual override {
-        _walletOfOwner[to].push(tokenId);
         super._mint(to, tokenId);
+        _walletOfOwner[to].push(tokenId);
     }
 }
