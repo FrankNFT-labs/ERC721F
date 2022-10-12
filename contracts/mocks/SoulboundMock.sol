efc9777221b55612dd9f8137c87a9cd58dba5714
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-12 16:57:29 +0200

    Added testing functions

--- contracts/mocks/SoulboundMock.sol
+++ contracts/mocks/SoulboundMock.sol
@@ -7,4 +7,12 @@ contract SoulboundMock is Soulbound {
     constructor(string memory name, string memory symbol)
         Soulbound(name, symbol)
     {}
+
+    function mint(string memory uri) public {
+        _mint(msg.sender, uri);
+    }
+
+    function burn(uint256 tokenId) public {
+        _burn(tokenId);
+    }
 }
