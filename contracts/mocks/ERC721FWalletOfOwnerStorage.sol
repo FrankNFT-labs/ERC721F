d583a78d595a9363344021aa7c46a1eb450e90dc
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-12 16:39:50 +0200

    Added basic documentation

--- contracts/mocks/ERC721FWalletOfOwnerStorage.sol
+++ contracts/mocks/ERC721FWalletOfOwnerStorage.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.8.9 <0.9.0;
 
 import "../token/ERC721/extensions/ERC721FWalletOfOwnerStorage.sol";
 
+/**
+ * @title ERC721FWalletOfOwnerStorageMock
+ * This mock provides a public mint and burn function for testin purposes
+ */
 contract ERC721FWalletOfOwnerStorageMock is ERC721FWalletOfOwnerStorage {
     constructor(string memory name, string memory symbol) ERC721F(name, symbol) {
         setBaseTokenURI("ipfs://QmVy7VQUFtTQawBsp4tbJPp9MgbTKS4L7WSDpZEdZUzsiD/"); 
     }
 
+    /**
+     * @notice Mint your tokens here
+     * @dev Function utilised in testing, don't use in production due to lack of restrictions
+     */
     function mint(uint256 numberOfTokens) public {
         for(uint256 i; i < numberOfTokens;){
             _mint( msg.sender, totalSupply()); // no need to use safeMint as we don't allow contracts.
@@ -15,6 +23,9 @@ contract ERC721FWalletOfOwnerStorageMock is ERC721FWalletOfOwnerStorage {
         }
     }
 
+    /**
+     * @notice Burns `tokenId`
+     */
     function burn(uint256 tokenId) public {
         _burn(tokenId);
     }
