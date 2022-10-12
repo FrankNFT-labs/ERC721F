095e6b74852f77aca2a2be968611d636580219be
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-12 14:34:44 +0200

    Add mock functions for testing

--- contracts/mocks/ERC721FWalletOfOwnerStorage.sol
+++ contracts/mocks/ERC721FWalletOfOwnerStorage.sol
@@ -5,6 +5,17 @@ import "../token/ERC721/extensions/ERC721FWalletOfOwnerStorage.sol";
 
 contract ERC721FWalletOfOwnerStorageMock is ERC721FWalletOfOwnerStorage {
     constructor(string memory name, string memory symbol) ERC721F(name, symbol) {
-        
+        setBaseTokenURI("ipfs://QmVy7VQUFtTQawBsp4tbJPp9MgbTKS4L7WSDpZEdZUzsiD/"); 
+    }
+
+    function mint(uint256 numberOfTokens) public {
+        for(uint256 i; i < numberOfTokens;){
+            _mint( msg.sender, totalSupply()); // no need to use safeMint as we don't allow contracts.
+            unchecked{ i++;}
+        }
+    }
+
+    function burn(uint256 tokenId) public {
+        _burn(tokenId);
     }
 }
