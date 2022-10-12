1794258fe7e5f7e5c61266d5aa559087c50fdb60
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-12 16:40:49 +0200

    Code format

--- contracts/mocks/ERC721FWalletOfOwnerStorage.sol
+++ contracts/mocks/ERC721FWalletOfOwnerStorage.sol
@@ -1,24 +1,27 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9 <0.9.0;
 
 import "../token/ERC721/extensions/ERC721FWalletOfOwnerStorage.sol";
 
 /**
  * @title ERC721FWalletOfOwnerStorageMock
- * This mock provides a public mint and burn function for testin purposes
+ * This mock provides a public mint and burn function for testing purposes
  */
 contract ERC721FWalletOfOwnerStorageMock is ERC721FWalletOfOwnerStorage {
-    constructor(string memory name, string memory symbol) ERC721F(name, symbol) {
-    }
+    constructor(string memory name, string memory symbol)
+        ERC721F(name, symbol)
+    {}
 
     /**
      * @notice Mint your tokens here
      * @dev Function utilised in testing, don't use in production due to lack of restrictions
      */
     function mint(uint256 numberOfTokens) public {
-        for(uint256 i; i < numberOfTokens;){
-            _mint( msg.sender, totalSupply()); // no need to use safeMint as we don't allow contracts.
-            unchecked{ i++;}
+        for (uint256 i; i < numberOfTokens; ) {
+            _mint(msg.sender, totalSupply());
+            unchecked {
+                i++;
+            }
         }
     }
 
