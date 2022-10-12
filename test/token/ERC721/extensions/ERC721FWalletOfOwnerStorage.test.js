c2dd60ed9a86c1fd8c0e89a351fda67467197cef
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-12 14:51:06 +0200

    Testing effect on walletOfOwner after transferring

--- test/token/ERC721/extensions/ERC721FWalletOfOwnerStorage.test.js
+++ test/token/ERC721/extensions/ERC721FWalletOfOwnerStorage.test.js
@@ -24,7 +24,7 @@ describe("ERC721FWalletOfOwnerStorage", function () {
                 ownerAddress = owner;
                 otherAddress = addr1;
                 await token.mint(2);
-            })
+            });
 
             it("Should assign the minted tokens to the wallet of the minter", async function() {
                 const walletOfOwner = await token.walletOfOwner(ownerAddress.address);
@@ -39,5 +39,39 @@ describe("ERC721FWalletOfOwnerStorage", function () {
                 expect(walletOfOwner.map(t => t.toNumber())).to.have.members([2, 3, 4, 5, 6]);
             });
         }); 
+
+        context("Transferring", function() {
+            let token;
+            let ownerAddress;
+            let otherAddress;
+
+            beforeEach(async () => {
+                const { hardhatToken, owner, addr1 } = await deployTokenFixture(loadFixture);   
+                token = hardhatToken;
+                ownerAddress = owner;
+                otherAddress = addr1;
+                await token.mint(1);
+                await token.connect(otherAddress).mint(2);
+                await token.connect(otherAddress).transferFrom(otherAddress.address, ownerAddress.address, 2);
+            });
+
+            it("Should add transferred token to the wallet of the receiver", async function() {
+                const walletOfOwner = await token.walletOfOwner(ownerAddress.address);
+
+                expect(walletOfOwner.map(t => t.toNumber())).to.have.members([0, 2]);
+            });
+
+            it("Should remove the transferred token from the wallet of the transferee", async function() {
+                const walletOfOwner = await token.walletOfOwner(otherAddress.address);
+
+                expect(walletOfOwner.map(t => t.toNumber())).to.not.have.members([2]);
+            });
+
+            it("Shouldn't remove non-transferred tokens from the wallet of the transferee", async function() {
+                const walletOfOwner = await token.walletOfOwner(otherAddress.address);
+
+                expect(walletOfOwner.map(t => t.toNumber())).to.have.members([1]);
+            });
+        });
     });
 });
