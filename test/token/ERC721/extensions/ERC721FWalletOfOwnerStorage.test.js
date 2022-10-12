ccb5f650996578d226320d2503969fd29fca562b
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-12 14:45:33 +0200

    Testing affect on walletOfOwner during minting

--- test/token/ERC721/extensions/ERC721FWalletOfOwnerStorage.test.js
+++ test/token/ERC721/extensions/ERC721FWalletOfOwnerStorage.test.js
@@ -11,4 +11,33 @@ describe("ERC721FWalletOfOwnerStorage", function () {
 
         return { Token, hardhatToken, owner, addr1 };
     }
+
+    describe("walletOfOwner", function() {
+        context("Minting", function() {
+            let token;
+            let ownerAddress;
+            let otherAddress;
+
+            beforeEach(async () => {
+                const { hardhatToken, owner, addr1 } = await deployTokenFixture(loadFixture);   
+                token = hardhatToken;
+                ownerAddress = owner;
+                otherAddress = addr1;
+                await token.mint(2);
+            })
+
+            it("Should assign the minted tokens to the wallet of the minter", async function() {
+                const walletOfOwner = await token.walletOfOwner(ownerAddress.address);
+                
+                expect(walletOfOwner.map(t => t.toNumber())).to.have.members([0, 1]);
+            });
+
+            it("Should assign the minted tokens to the minter address", async function() {
+                await token.connect(otherAddress).mint(5);
+                const walletOfOwner = await token.walletOfOwner(otherAddress.address);
+
+                expect(walletOfOwner.map(t => t.toNumber())).to.have.members([2, 3, 4, 5, 6]);
+            });
+        }); 
+    });
 });
