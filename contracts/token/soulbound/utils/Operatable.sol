e91c5add46c6f7d186688d48cca3bc0733cab158
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-14 14:17:52 +0200

    Added basic documentation

--- contracts/token/soulbound/utils/Operatable.sol
+++ contracts/token/soulbound/utils/Operatable.sol
@@ -7,6 +7,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Operatable is Ownable, AccessControl {
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
 
+    /**
+     * @dev Only operators can pass modifier
+     */
     modifier onlyOperator() {
         require(hasRole(OPERATOR_ROLE, msg.sender), "Sender does not have operator role");
         _;
@@ -17,14 +20,23 @@ contract Operatable is Ownable, AccessControl {
         _setupRole(OPERATOR_ROLE, msg.sender);
     }
 
+    /**
+     * @dev Assign `_account` `OPERATOR_ROLE`
+     */
     function addOperator(address _account) public onlyOwner {
         grantRole(OPERATOR_ROLE, _account);
     }
 
+    /**
+     * @dev Remove `_account` from `OPERATOR_ROLE`
+     */
     function removeOperator(address _account) public onlyOwner {
         revokeRole(OPERATOR_ROLE, _account);
     }
 
+    /**
+     * @dev Returns `true` if `_account` has `OPERATOR_ROLE`, otherwise returns `false` 
+     */
     function checkOperator(address _account) public view returns (bool) { 
         return hasRole(OPERATOR_ROLE, _account);
     }
