4f04b298893cebc03a0143003c1ca3a1efa68d31
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-18 10:26:45 +0200

    Code format

--- contracts/mocks/ERC721FMock.sol
+++ contracts/mocks/ERC721FMock.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9 <0.9.0;
 
 import "../token/ERC721/ERC721F.sol";
--- examples/FreeMint.sol
+++ examples/FreeMint.sol
@@ -1,22 +1,22 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9 <0.9.0;
 
 import "../contracts/token/ERC721/ERC721F.sol";
 
 /**
  * @title FreeMint
- * 
+ *
  * @dev Example implementation of [ERC721F]
  */
 contract FreeMint is ERC721F {
-
     uint256 public constant MAX_TOKENS = 10000;
     uint public constant MAX_PURCHASE = 31; // Theoretical limit 1100
     bool public saleIsActive;
 
-    
     constructor() ERC721F("FreeMint", "Free") {
-        setBaseTokenURI("ipfs://QmVy7VQUFtTQawBsp4tbJPp9MgbTKS4L7WSDpZEdZUzsiD/"); 
+        setBaseTokenURI(
+            "ipfs://QmVy7VQUFtTQawBsp4tbJPp9MgbTKS4L7WSDpZEdZUzsiD/"
+        );
     }
 
     /**
@@ -30,15 +30,23 @@ contract FreeMint is ERC721F {
      * Mint your tokens here.
      */
     function mint(uint256 numberOfTokens) external {
-        require(msg.sender==tx.origin,"No Contracts allowed.");
-        require(saleIsActive,"Sale NOT active yet");
+        require(msg.sender == tx.origin, "No Contracts allowed.");
+        require(saleIsActive, "Sale NOT active yet");
         require(numberOfTokens != 0, "numberOfNfts cannot be 0");
-        require(numberOfTokens < MAX_PURCHASE, "Can only mint 30 tokens at a time");
+        require(
+            numberOfTokens < MAX_PURCHASE,
+            "Can only mint 30 tokens at a time"
+        );
         uint256 supply = totalSupply();
-        require(supply + numberOfTokens <= MAX_TOKENS, "Purchase would exceed max supply of Tokens");
-        for(uint256 i; i < numberOfTokens;){
-            _mint( msg.sender, supply + i ); // no need to use safeMint as we don't allow contracts.
-            unchecked{ i++;}
+        require(
+            supply + numberOfTokens <= MAX_TOKENS,
+            "Purchase would exceed max supply of Tokens"
+        );
+        for (uint256 i; i < numberOfTokens; ) {
+            _mint(msg.sender, supply + i); // no need to use safeMint as we don't allow contracts.
+            unchecked {
+                i++;
+            }
         }
     }
 }
