ec943990a5c091287a9e2cd89a07441218a7ddb5
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-18 15:04:30 +0200

    Testing deployment settings

--- test/examples/FreeMint.test.js
+++ test/examples/FreeMint.test.js
@@ -14,7 +14,33 @@ describe("FreeMint", function () {
         return { Token, hardhatToken, owner, addr1 };
     }
 
-    describe("Max tokens minted in ONE TRX", function () {
+    describe("Deployment", function() {
+        it("Supports ERC721 standards", async function() {
+            const ERC721InterfaceId = 0x80ac58cd;
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            expect(await hardhatToken.supportsInterface(ERC721InterfaceId)).to.be.true;
+        });
+
+        it("Supports ERC2981 standards", async function() {
+            const ERC2981InterfaceId = 0x2a55205a;
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            expect(await hardhatToken.supportsInterface(ERC2981InterfaceId)).to.be.true;
+        });
+
+        it("Has royalties at 5% by default", async function() {
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+
+            await hardhatToken.flipSaleState();
+            await hardhatToken.mint(1);
+
+            const { royaltyAmount } = await hardhatToken.royaltyInfo(0, 100);
+            expect(royaltyAmount).to.be.equal(5);
+        });
+    });
+
+    describe.skip("Max tokens minted in ONE TRX", function () {
         let totalMint = 1120; // Lower limit of tokens that'll increase in amount and be minted
         this.retries(10); // Amount of times the test will be attempted after failure
 
