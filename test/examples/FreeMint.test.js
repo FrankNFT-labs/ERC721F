e893c3ae5876211fc72e4c46c5347c1ad7cb7d3b
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-18 10:21:37 +0200

    Re-implemented test of theoretical max tokens minted in a single transaction

--- test/examples/FreeMint.test.js
+++ test/examples/FreeMint.test.js
@@ -13,4 +13,21 @@ describe("FreeMint", function () {
 
         return { Token, hardhatToken, owner, addr1 };
     }
+
+    describe("Max tokens minted in ONE TRX", function () {
+        let totalMint = 1120; // Lower limit of tokens that'll increase in amount and be minted
+        this.retries(10); // Amount of times the test will be attempted after failure
+
+        beforeEach(function () {
+            totalMint = totalMint + 1; // Increase total amount of tokens that are getting minted
+        })
+
+        // Test passes when `totalMint` is high enough to transcend the gas limit of a single transaction, this amount will then be displayed in the console 
+        it("Should eventually fail indicating the total tokens minted", async function () {
+            const { hardhatToken } = await loadFixture(deployTokenFixture);
+            await hardhatToken.flipSaleState();
+            await expect(hardhatToken.mint(totalMint)).to.be.reverted;
+            console.log(totalMint);
+        })
+    });
 });
