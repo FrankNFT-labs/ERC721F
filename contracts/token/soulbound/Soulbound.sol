c70dd53fa326eff16d4db3753f6d5174f753449b
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-18 13:27:38 +0200

    Removed console.sol import

--- contracts/token/soulbound/Soulbound.sol
+++ contracts/token/soulbound/Soulbound.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.9 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
 
 contract Soulbound is ERC721, ERC721URIStorage, Ownable {
     uint256 _tokenSupply;
