0e0afd5f9155623718ce4b0f831af7dd64a30959
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-21 10:27:17 +0200

    Setup variables of contract

--- examples/AllowListWithAmount.sol
+++ examples/AllowListWithAmount.sol
@@ -5,6 +5,11 @@ import "../contracts/token/ERC721/ERC721FCOMMON.sol";
 import "../contracts/utils/AllowListWithAmount.sol";
 
 contract AllowListWithAmountExample is ERC721FCOMMON, AllowListWithAmount {
+    uint256 public constant MAX_TOKENS = 10000;
+    uint public constant MAX_PURCHASE = 31;
+    bool public preSaleIsActive;
+    bool public saleIsActive;
+    
     constructor() ERC721FCOMMON("AllowListWithAmount", "ALA") {
         setBaseTokenURI(
             "ipfs://QmVy7VQUFtTQawBsp4tbJPp9MgbTKS4L7WSDpZEdZUzsiD/"
