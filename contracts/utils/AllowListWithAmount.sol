fc20d70c53c25f98707f82356edd3f6436170b8c
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-21 10:19:03 +0200

    Setup onlyAllowListWithAvaiableTokens modifier which uses allowList to revert requests by addresses with zero tokens in allowList

--- contracts/utils/AllowListWithAmount.sol
+++ contracts/utils/AllowListWithAmount.sol
@@ -4,4 +4,14 @@ pragma solidity ^0.8.9 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 abstract contract AllowList is Ownable {
+    mapping(address => uint256) private allowList;
+
+    modifier onlyAllowListWithAvailableTokens() {
+        require(getAllowListFunds(msg.sender) > 0, "Address does not have any tokens available within allowList");
+        _;
+    }
+
+    function getAllowListFunds(address _address) public view returns (uint256) {
+        return allowList[_address];
+    }
 }
