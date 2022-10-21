85a89c13ebd44731aefe5d4e919c73cb77220977
HansRobrecht <hans.robrecht@student.odisee.be>
2022-10-21 10:35:37 +0200

    Copied over main functionality from AllowList example

--- examples/AllowListWithAmount.sol
+++ examples/AllowListWithAmount.sol
@@ -15,4 +15,55 @@ contract AllowListWithAmountExample is ERC721FCOMMON, AllowListWithAmount {
             "ipfs://QmVy7VQUFtTQawBsp4tbJPp9MgbTKS4L7WSDpZEdZUzsiD/"
         );
     }
+
+    modifier validMintRequest(uint256 numberOfTokens) {
+        require(numberOfTokens != 0, "numberOfNfts cannot be 0");
+        require(
+            numberOfTokens < MAX_PURCHASE,
+            "Can only mint 30 tokens at a time"
+        );
+        require(
+            tokenPrice * numberOfTokens <= msg.value,
+            "Ether value sent is not correct"
+        );
+        _;
+    }
+    
+    function flipPreSaleState() external onlyOwner {
+        preSaleIsActive = !preSaleIsActive;
+    }
+
+    function flipSaleState() external onlyOwner {
+        saleIsActive = !saleIsActive;
+        if (saleIsActive) {
+            preSaleIsActive = false;
+        }
+    }
+
+    function mint(uint256 numberOfTokens)
+        external
+        payable
+        validMintRequest(numberOfTokens)
+    {
+        require(msg.sender == tx.origin, "No contracts allowed");
+        require(saleIsActive, "Sale NOT active yet");
+        uint256 supply = _totalMinted();
+        require(
+            supply + numberOfTokens <= MAX_TOKENS,
+            "Purchase would exceed max supply of tokens"
+        );
+
+        for (uint256 i; i < numberOfTokens; ) {
+            _mint(msg.sender, supply + i);
+            unchecked {
+                i++;
+            }
+        }
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Insufficient balance");
+        _withdraw(owner(), balance);
+    }
 }
