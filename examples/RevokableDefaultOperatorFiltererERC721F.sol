605de6e199e7bad99bfacca7f0c63671b731dae9
HansRobrecht <hans.robrecht@student.odisee.be>
2022-11-30 10:33:16 +0100

    Copied over OpenseaExample code and AllowList paid mint code

--- examples/RevokableDefaultOperatorFiltererERC721F.sol
+++ examples/RevokableDefaultOperatorFiltererERC721F.sol
@@ -7,5 +7,131 @@ import "operator-filter-registry/src/RevokableDefaultOperatorFilterer.sol";
 import "operator-filter-registry/src/UpdatableOperatorFilterer.sol";
 
 contract RevokableDefaultOperatorFiltererERC721F is ERC721FCOMMON, AllowList, RevokableDefaultOperatorFilterer {
-    
+    uint256 public constant MAX_TOKENS = 10000;
+    uint public constant MAX_PURCHASE = 31;
+    uint public tokenPrice = 1 ether;
+    bool public preSaleIsActive;
+    bool public saleIsActive;
+
+    constructor() ERC721FCOMMON("RevokableDefaultOperatorFiltererERC721F", "RDOF") {
+        setBaseTokenURI(
+            "ipfs://QmVy7VQUFtTQawBsp4tbJPp9MgbTKS4L7WSDpZEdZUzsiD/"
+        );
+    }
+
+    modifier validMintRequest(uint256 numberOfTokens) {
+        require(numberOfTokens != 0, "numberOfNfts cannot be 0");
+        require(
+            numberOfTokens < MAX_PURCHASE,
+            "Can only mint 30 tokens at a time"
+        );
+        require(
+            tokenPrice * numberOfTokens <= msg.value,
+            "Ether value sent is not correct"
+        );
+        _;
+    }
+
+    /**
+     * @notice Changes the state of preSaleIsActive from true to false and false to true
+     */
+    function flipPreSaleState() external onlyOwner {
+        preSaleIsActive = !preSaleIsActive;
+    }
+
+    /**
+     * @notice Changes the state of saleIsActive from true to false and false to true
+     * @dev If saleIsActive becomes `true` sets preSaleIsActive to `false`
+     */
+    function flipSaleState() external onlyOwner {
+        saleIsActive = !saleIsActive;
+        if (saleIsActive) {
+            preSaleIsActive = false;
+        }
+    }
+
+    /**
+     * @notice Mints a certain number of tokens
+     * @param numberOfTokens Total tokens to be minted, must be larger than 0 and at most 30
+     */
+    function mint(uint256 numberOfTokens)
+        external
+        payable
+        validMintRequest(numberOfTokens)
+    {
+        require(msg.sender == tx.origin, "No contracts allowed");
+        require(saleIsActive, "Sale NOT active yet");
+        uint256 supply = _totalMinted();
+        require(
+            supply + numberOfTokens <= MAX_TOKENS,
+            "Purchase would exceed max supply of tokens"
+        );
+
+        for (uint256 i; i < numberOfTokens; ) {
+            _mint(msg.sender, supply + i);
+            unchecked {
+                i++;
+            }
+        }
+    }
+
+    /**
+     * @notice Mints a certain number of tokens
+     * @param numberOfTokens Total tokens to be minted, must be larger than 0 and at most 30
+     * @dev Uses AllowList.onlyAllowList modifier for whitelist functionality
+     */
+    function mintPreSale(uint256 numberOfTokens)
+        external
+        payable
+        validMintRequest(numberOfTokens)
+        onlyAllowList
+    {
+        require(preSaleIsActive, "PreSale is NOT active yet");
+        uint256 supply = _totalMinted();
+        require(
+            supply + numberOfTokens <= MAX_TOKENS,
+            "Purchase would exceed max supply of tokens"
+        );
+
+        for (uint256 i; i < numberOfTokens; ) {
+            _safeMint(msg.sender, supply + i);
+            unchecked {
+                i++;
+            }
+        }
+    }
+
+    function withdraw() external onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Insufficient balance");
+        _withdraw(owner(), balance);
+    }
+
+    function setApprovalForAll(address operator, bool approved) public override onlyAllowedOperatorApproval(operator) {
+        super.setApprovalForAll(operator, approved);
+    }
+
+    function approve(address operator, uint256 tokenId) public override onlyAllowedOperatorApproval(operator) {
+        super.approve(operator, tokenId);
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.transferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, tokenId);
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data)
+        public
+        override
+        onlyAllowedOperator(from)
+    {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+
+    function owner() public view virtual override(Ownable, UpdatableOperatorFilterer) returns (address) {
+        return Ownable.owner();
+    }
 }
