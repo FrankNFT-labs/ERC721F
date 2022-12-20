f45b39b5c153a0611633cd90a24d05b0d42c2530
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-20 15:24:12 +0100

    Creating four separate snapshot for each section

--- scripts/bat/generate-snapshots.bat
+++ scripts/bat/generate-snapshots.bat
@@ -1,2 +1,5 @@
 @echo off
-forge snapshot --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint.gas-snapshot --match-test '\W*(testMint)\W*'
+forge snapshot --asc --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint.gas-snapshot --match-test \W*(testMint)\W*
+forge snapshot --asc --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint-1-Transfer.gas-snapshot --match-test \W*(TransferMintOne)\W*
+forge snapshot --asc --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint-10-Transfer.gas-snapshot --match-test \W*(TransferMintTen)\W*
+forge snapshot --asc --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint-100-Transfer.gas-snapshot --match-test \W*(TransferMintHundred)\W*
