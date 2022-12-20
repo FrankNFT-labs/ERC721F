05d23a37e6143de599d4a48d933295fd831de93f
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-20 15:58:15 +0100

    Now printing the results of the gas report to the four snapshots instead

--- scripts/bat/generate-snapshots.bat
+++ scripts/bat/generate-snapshots.bat
@@ -1,5 +1,5 @@
-@echo off
-forge snapshot --asc --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint.gas-snapshot --match-test \W*(testMint)\W*
-forge snapshot --asc --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint-1-Transfer.gas-snapshot --match-test \W*(TransferMintOne)\W*
-forge snapshot --asc --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint-10-Transfer.gas-snapshot --match-test \W*(TransferMintTen)\W*
-forge snapshot --asc --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --snap forge-snapshots/mint-100-Transfer.gas-snapshot --match-test \W*(TransferMintHundred)\W*
+cls
+forge test --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --match-test \W*(testMint)\W* --gas-report > forge-snapshots/mint.gas-snapshot
+forge test --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --match-test \W*(TransferMintOne)\W* --gas-report > forge-snapshots/mint-1-Transfer.gas-snapshot
+forge test --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --match-test \W*(TransferMintTen)\W* --gas-report > forge-snapshots/mint-10-Transfer.gas-snapshot
+forge test --match-path test\foundry\token\ERC721\ERC721FGasReporterMock.t.sol --match-test \W*(TransferMintHundred)\W* --gas-report > forge-snapshots/mint-100-Transfer.gas-snapshot
