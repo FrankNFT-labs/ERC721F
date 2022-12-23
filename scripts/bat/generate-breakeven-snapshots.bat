b5c396d230a17f9d4e28438b6b35f405dc61bdae
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-23 11:33:42 +0100

    Add echoes indicating progress of script

--- scripts/bat/generate-breakeven-snapshots.bat
+++ scripts/bat/generate-breakeven-snapshots.bat
@@ -1,13 +1,21 @@
 @echo off
+cls
 
 for /L %%i in (0,10,100) do (
     if %%i == 0 (
         node .\scripts\js\update_env_breakeven_count.js 1
+        echo ^> Began updating .env with value 1
     ) else (
         node .\scripts\js\update_env_breakeven_count.js %%i
+        echo ^> Began updating .env with value %%i
     )
-    forge test --mc \bBreakEven\b --gas-report > forge-snapshots/temp.gas-snapshot
+    echo Finished updating .env
 
+    echo ^> Began executing BreakEven tests
+    forge test --mc \bBreakEven\b --gas-report > forge-snapshots/temp.gas-snapshot
+    echo Finished executing tests
+
+    echo ^> Began writing results to respective files
     if %%i == 0 (
         for /f "delims=" %%i in ('findstr /C:"Function Name" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i > forge-snapshots/BreakEven-ascended.gas.snapshot)
         for /f "delims=" %%i in ('findstr /C:"Function Name" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i > forge-snapshots/BreakEven-descended.gas.snapshot)
@@ -26,6 +34,9 @@ for /L %%i in (0,10,100) do (
     )
     for /f "delims=" %%i in ('findstr /C:"transferAsc" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i >> forge-snapshots/BreakEven-ascended.gas.snapshot)
     for /f "delims=" %%i in ('findstr /C:"transferDesc" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i >> forge-snapshots/BreakEven-descended.gas.snapshot)
+    echo Finished writing results
+
+    echo.
 )
 
 del forge-snapshots\temp.gas-snapshot
