0f628880b1f3ed24fb7f5a37a66c01f07aa6a977
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-23 13:31:58 +0100

    Now using .js files to alter foundry.toml state to run 3 separate optimizer scenarios

--- scripts/bat/generate-gasusage-snapshots.bat
+++ scripts/bat/generate-gasusage-snapshots.bat
@@ -1,40 +1,67 @@
 @echo off
 cls
+setlocal enabledelayedexpansion
 
-del forge-snapshots\ERC721FGasReporterMock.gas-snapshot
+set SNAPSHOT_FILES[0]=ERC721FGasReporterMock-optimizer-disabled.gas-snapshot
+set SNAPSHOT_FILES[1]=ERC721FGasReporterMock-optimizer-200.gas-snapshot
+set SNAPSHOT_FILES[2]=ERC721FGasReporterMock-optimizer-1000.gas-snapshot
 
-echo ^> Began testing of mint
-forge test --mc \bERC721FGasReporterMockTest\b --match-test \W*(testMint)\W* --gas-report > forge-snapshots/temp.gas-snapshot
-echo Finished mint tests
-echo ^> Began writing results in respective file
-node scripts/js/cleanup_snapshot.js forge-snapshots/temp.gas-snapshot forge-snapshots/ERC721FGasReporterMock.gas-snapshot
-echo Finished writing results
+if exist forge-snapshots\ERC721FGasReporterMock-optimizer-disabled.gas-snapshot del forge-snapshots\ERC721FGasReporterMock-optimizer-disabled.gas-snapshot
+if exist forge-snapshots\ERC721FGasReporterMock-optimizer-200.gas-snapshot del forge-snapshots\ERC721FGasReporterMock-optimizer-200.gas-snapshot
+if exist forge-snapshots\ERC721FGasReporterMock-optimizer-1000.gas-snapshot del forge-snapshots\ERC721FGasReporterMock-optimizer-1000.gas-snapshot
 
-echo.
+for /L %%i in (0,1,2) do (
+    set SNAPSHOT_LOCATION=forge-snapshots/!SNAPSHOT_FILES[%%i]!
 
-echo ^> Began testing of transfers with 1 token minted
-forge test --mc \bERC721FGasReporterMockTest\b --match-test \W*(TransferMintOne)\W* --gas-report > forge-snapshots/temp.gas-snapshot
-echo Fininshed transfer tests
-echo ^> Began writing results in respective file
-node scripts/js/cleanup_snapshot.js forge-snapshots/temp.gas-snapshot forge-snapshots/ERC721FGasReporterMock.gas-snapshot
-echo Finished writing results
+    if %%i == 0 (
+        node .\scripts\js\update_foundry_optimizer.js false
+        echo == Optimizer Disabled ==
+    ) else (
+        node .\scripts\js\update_foundry_optimizer.js true
+        if %%i == 1 (
+            node .\scripts\js\update_foundry_optimizer_runs.js 200
+            echo == Optimizer - 200 runs ==
+        ) else (
+            node .\scripts\js\update_foundry_optimizer_runs.js 1000
+            echo == Optimizer - 1000 runs ==
+        )
+    )
 
-echo.
+    echo ^> Began testing of mint
+    forge test --mc \bERC721FGasReporterMockTest\b --match-test \W*^(testMint^)\W* --gas-report > forge-snapshots/temp.gas-snapshot
+    echo Finished mint tests
+    echo ^> Began writing results in respective file
+    node scripts/js/cleanup_snapshot.js forge-snapshots/temp.gas-snapshot !SNAPSHOT_LOCATION!
+    echo Finished writing results
 
-echo ^> Began testing of transfers with 10 tokens minted
-forge test --mc \bERC721FGasReporterMockTest\b --match-test \W*(TransferMintTen)\W* --gas-report > forge-snapshots/temp.gas-snapshot
-echo Finished transfer tests
-echo ^> Began writing results in respective files
-node scripts/js/cleanup_snapshot.js forge-snapshots/temp.gas-snapshot forge-snapshots/ERC721FGasReporterMock.gas-snapshot
-echo Finished writing results
+    echo.
 
-echo.
+    echo ^> Began testing of transfers with 1 token minted
+    forge test --mc \bERC721FGasReporterMockTest\b --match-test \W*^(TransferMintOne^)\W* --gas-report > forge-snapshots/temp.gas-snapshot
+    echo Fininshed transfer tests
+    echo ^> Began writing results in respective file
+    node scripts/js/cleanup_snapshot.js forge-snapshots/temp.gas-snapshot !SNAPSHOT_LOCATION!
+    echo Finished writing results
 
-echo ^> Began testing of transfers with 100 tokens minted
-forge test --mc \bERC721FGasReporterMockTest\b --match-test \W*(TransferMintHundred)\W* --gas-report > forge-snapshots/temp.gas-snapshot
-echo Finished transfer tests
-echo ^> Began writing results in respective files
-node scripts/js/cleanup_snapshot.js forge-snapshots/temp.gas-snapshot forge-snapshots/ERC721FGasReporterMock.gas-snapshot
-echo Finished writing results
+    echo.
+
+    echo ^> Began testing of transfers with 10 tokens minted
+    forge test --mc \bERC721FGasReporterMockTest\b --match-test \W*^(TransferMintTen^)\W* --gas-report > forge-snapshots/temp.gas-snapshot
+    echo Finished transfer tests
+    echo ^> Began writing results in respective files
+    node scripts/js/cleanup_snapshot.js forge-snapshots/temp.gas-snapshot !SNAPSHOT_LOCATION!
+    echo Finished writing results
+
+    echo.
+
+    echo ^> Began testing of transfers with 100 tokens minted
+    forge test --mc \bERC721FGasReporterMockTest\b --match-test \W*^(TransferMintHundred^)\W* --gas-report > forge-snapshots/temp.gas-snapshot
+    echo Finished transfer tests
+    echo ^> Began writing results in respective files
+    node scripts/js/cleanup_snapshot.js forge-snapshots/temp.gas-snapshot !SNAPSHOT_LOCATION!
+    echo Finished writing results
+    
+    echo.
+)
 
 del forge-snapshots\temp.gas-snapshot
