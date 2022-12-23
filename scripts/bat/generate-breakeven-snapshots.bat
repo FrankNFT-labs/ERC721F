d3401776522d8d18c287728e2a8f480693b06dfc
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-23 10:41:10 +0100

    Made single delete from double delete and added column headers

--- scripts/bat/generate-breakeven-snapshots.bat
+++ scripts/bat/generate-breakeven-snapshots.bat
@@ -1,8 +1,5 @@
 @echo off
 
-del forge-snapshots\BreakEven-ascended.gas.snapshot
-del forge-snapshots\BreakEven-descended.gas.snapshot
-
 for /L %%i in (0,10,100) do (
     if %%i == 0 (
         node .\scripts\js\update_env_breakeven_count.js 1
@@ -12,9 +9,13 @@ for /L %%i in (0,10,100) do (
     forge test --mc \bBreakEven\b --gas-report > forge-snapshots/temp.gas-snapshot
 
     if %%i == 0 (
+        findstr /C:"Function Name" .\forge-snapshots\temp.gas-snapshot | find /V "" > forge-snapshots/BreakEven-ascended.gas.snapshot
+        findstr /C:"Function Name" .\forge-snapshots\temp.gas-snapshot | find /V "" > forge-snapshots/BreakEven-descended.gas.snapshot
         findstr /C:"mintHundred" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-ascended.gas.snapshot
         findstr /C:"mintHundred" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-descended.gas.snapshot
     )
     findstr /C:"transferAsc" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-ascended.gas.snapshot
     findstr /C:"transferDesc" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-descended.gas.snapshot
 )
+
+del forge-snapshots\temp.gas-snapshot
