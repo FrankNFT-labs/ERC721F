473a751be0c6d7b86a70202ef45964333e9bedd3
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-23 13:54:31 +0100

    Now creating directory in case it doesn't exist

--- scripts/js/cleanup_snapshot.js
+++ scripts/js/cleanup_snapshot.js
@@ -4,6 +4,18 @@ const wordCombination = 'Test result:';
 const inputFilename = process.argv[2];
 const outputFilename = process.argv[3];
 
+const directory = outputFilename.substring(0, outputFilename.lastIndexOf('/'));
+const file = outputFilename.substring(outputFilename.lastIndexOf('/') + 1);
+
+try {
+  fs.mkdirSync(directory, { recursive: true });
+} catch (err) {
+  if (err.code !== 'EEXIST') {
+    console.error(err);
+    return;
+  }
+}
+
 fs.readFile(inputFilename, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
