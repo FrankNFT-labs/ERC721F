13bfefc44ccef71997490c6df69ce75003bcb320
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-23 10:34:42 +0100

    Now outputting respective results in snapshot files

--- scripts/bat/generate-breakeven-snapshots.bat
+++ scripts/bat/generate-breakeven-snapshots.bat
@@ -1,5 +1,8 @@
 @echo off
 
+del forge-snapshots\BreakEven-ascended.gas.snapshot
+del forge-snapshots\BreakEven-descended.gas.snapshot
+
 for /L %%i in (0,10,100) do (
     if %%i == 0 (
         node .\scripts\js\update_env_breakeven_count.js 1
@@ -7,4 +10,11 @@ for /L %%i in (0,10,100) do (
         node .\scripts\js\update_env_breakeven_count.js %%i
     )
     forge test --mc \bBreakEven\b --gas-report > forge-snapshots/temp.gas-snapshot
+
+    if %%i == 0 (
+        findstr /C:"mintHundred" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-ascended.gas.snapshot
+        findstr /C:"mintHundred" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-descended.gas.snapshot
+    )
+    findstr /C:"transferAsc" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-ascended.gas.snapshot
+    findstr /C:"transferDesc" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-descended.gas.snapshot
 )
