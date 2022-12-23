fe9947d81c14224ba54ebc47bef1cce696a4ecab
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-23 09:25:00 +0100

    Made .bat file to update .env value

---
+++ scripts/bat/generate-breakeven-snapshots.bat
@@ -0,0 +1,9 @@
+@echo off
+
+for /L %%i in (0,10,100) do (
+    if %%i == 0 (
+        node .\scripts\js\update_env_breakeven_count.js 1
+    ) else (
+        node .\scripts\js\update_env_breakeven_count.js %%i
+    )
+)
