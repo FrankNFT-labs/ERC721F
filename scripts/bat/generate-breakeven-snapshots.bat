f9ab3feb8de46c9115e652a7e8bd3bc81a8baf52
HansRobrecht <hans.robrecht@student.odisee.be>
2022-12-23 10:53:42 +0100

    Added spacing between values and indications of total transfers

--- scripts/bat/generate-breakeven-snapshots.bat
+++ scripts/bat/generate-breakeven-snapshots.bat
@@ -9,13 +9,23 @@ for /L %%i in (0,10,100) do (
     forge test --mc \bBreakEven\b --gas-report > forge-snapshots/temp.gas-snapshot
 
     if %%i == 0 (
-        findstr /C:"Function Name" .\forge-snapshots\temp.gas-snapshot | find /V "" > forge-snapshots/BreakEven-ascended.gas.snapshot
-        findstr /C:"Function Name" .\forge-snapshots\temp.gas-snapshot | find /V "" > forge-snapshots/BreakEven-descended.gas.snapshot
-        findstr /C:"mintHundred" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-ascended.gas.snapshot
-        findstr /C:"mintHundred" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-descended.gas.snapshot
+        for /f "delims=" %%i in ('findstr /C:"Function Name" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i > forge-snapshots/BreakEven-ascended.gas.snapshot)
+        for /f "delims=" %%i in ('findstr /C:"Function Name" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i > forge-snapshots/BreakEven-descended.gas.snapshot)
+        for /f "delims=" %%i in ('findstr /C:"mintHundred" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i >> forge-snapshots/BreakEven-ascended.gas.snapshot)
+        for /f "delims=" %%i in ('findstr /C:"mintHundred" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i >> forge-snapshots/BreakEven-descended.gas.snapshot)
+
+        echo. >> forge-snapshots/BreakEven-ascended.gas.snapshot
+        echo. >> forge-snapshots/BreakEven-descended.gas.snapshot
+        echo Tokens transfered 1 >> forge-snapshots/BreakEven-ascended.gas.snapshot
+        echo Tokens transfered 1 >> forge-snapshots/BreakEven-descended.gas.snapshot
+    ) else (
+        echo. >> forge-snapshots/BreakEven-ascended.gas.snapshot
+        echo. >> forge-snapshots/BreakEven-descended.gas.snapshot
+        echo Tokens transfered %%i >> forge-snapshots/BreakEven-ascended.gas.snapshot
+        echo Tokens transfered %%i >> forge-snapshots/BreakEven-descended.gas.snapshot
     )
-    findstr /C:"transferAsc" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-ascended.gas.snapshot
-    findstr /C:"transferDesc" .\forge-snapshots\temp.gas-snapshot | find /V "" >> forge-snapshots/BreakEven-descended.gas.snapshot
+    for /f "delims=" %%i in ('findstr /C:"transferAsc" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i >> forge-snapshots/BreakEven-ascended.gas.snapshot)
+    for /f "delims=" %%i in ('findstr /C:"transferDesc" .\forge-snapshots\temp.gas-snapshot') do (echo       %%i >> forge-snapshots/BreakEven-descended.gas.snapshot)
 )
 
 del forge-snapshots\temp.gas-snapshot
