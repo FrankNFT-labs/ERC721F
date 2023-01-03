3e88258bbfc73d66e24608d9d7f991f728b2caee
HansRobrecht <hans.robrecht@student.odisee.be>
2023-01-03 09:56:44 +0100

    Added referenced source

--- contracts/interfaces/IERC4883.sol
+++ contracts/interfaces/IERC4883.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9 <0.9.0;
 import "@openzeppelin/contracts/interfaces/IERC165.sol";
 
 /// @title EIP-4883 Non-Fungible Token Standard
+/// Based on https://eips.ethereum.org/EIPS/eip-4883
 interface IERC4883 is IERC165 {
     /**
      * @dev Generates the SVG image of `id`
