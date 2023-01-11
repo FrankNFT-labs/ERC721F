f1ba9eaa720225ccbb9f7d11f2455e6c115887d4
HansRobrecht <hans.robrecht@student.odisee.be>
2023-01-11 17:00:07 +0100

    Made foundry test for RequireNonZero

---
+++ test/foundry/examples/gas-optimizations/RequireNonZero.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9 <0.9.0;
+
+import "../../../../examples/gas-optimizations/RequireNonZero.sol";
+import "../../../../lib/forge-std/src/Test.sol";
+
+contract RequireNonZeroTest is Test {
+    RequireNonZero t;
+
+    function setUp() public {
+        t = new RequireNonZero();
+    }
+
+    function testRequireLargerThanZeroReverts() public {
+        vm.expectRevert(bytes("Value must be larger than 0"));
+        t.requireLargerThanZero(0);
+    }
+
+    function testRequireNotEqualsZeroReverts() public {
+        vm.expectRevert(bytes("Value, can't be 0"));
+        t.requireNotEqualsZero(0);
+    }
+}
