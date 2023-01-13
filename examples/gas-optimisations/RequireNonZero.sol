3daadd64e99e71f0b6b2faea8ca02f6e7642721b
HansRobrecht <hans.robrecht@student.odisee.be>
2023-01-13 10:02:54 +0100

    Add documentation explaining purpose behind contract

--- examples/gas-optimisations/RequireNonZero.sol
+++ examples/gas-optimisations/RequireNonZero.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9 <0.9.0;
 
+/**
+ * @title RequireNonZero
+ *
+ * @dev Contract to test gas consumption of require non-zero value statements
+ */
 contract RequireNonZero {
     function requireLargerThanZero(uint256 value) public pure {
         require(value > 0, "Value must be larger than 0");
