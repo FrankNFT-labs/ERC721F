71b7c1915848df04133ace1accca5e92a73d083b
HansRobrecht <125028885+N1h1lum@users.noreply.github.com>
2023-02-10 13:13:49 +0100

    Made ERC4906 example contract

---
+++ examples/ERC4906.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9 <0.9.0;
+
+import "../contracts/token/ERC721/ERC721F.sol";
+import "../contracts/interfaces/IERC4906.sol";
+
+contract ERC4906 is ERC721F, IERC4906 {
+    
+}
