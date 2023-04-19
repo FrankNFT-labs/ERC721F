b6d875b9a6e9acc543a4f8a6b1851245adfedab6
N1h1lum <zeronihilum@gmail.com>
2023-04-19 15:40:37 +0200

    Add documentation

--- examples/ERC721FVerifyImplementation.sol
+++ examples/ERC721FVerifyImplementation.sol
@@ -4,9 +4,18 @@ pragma solidity ^0.8.9 <0.9.0;
 import "@franknft.eth/erc721-f/contracts/token/ERC721/ERC721F.sol";
 import "./ERC721FVerify.sol";
 
+/**
+ * @title ERC721FVerifyImplementation
+ * @dev Example implemenation of ERC721FVerify to only allow mints when owning tokens in FreeMint
+ */
 contract ERC721FVerifyImplementation is ERC721FVerify, ERC721F {
     address public immutable FREEMINT_CONTRACT;
 
+    /**
+     * @param _warmContract address to deployed HotWalletProxy
+     * @param _delegateCashContract address to deployed DelegationRegistry
+     * @param _freeMintContract address to deployed FreeMint
+     */
     constructor(
         address _warmContract,
         address _delegateCashContract,
@@ -18,6 +27,11 @@ contract ERC721FVerifyImplementation is ERC721FVerify, ERC721F {
         FREEMINT_CONTRACT = _freeMintContract;
     }
 
+    /**
+     * @notice Mints `tokenId`
+     * @param TokenId to be minted
+     * @dev msg.sender must have tokens within the FreeMint contract
+     */
     function mint(uint256 tokenId) external {
         require(
             ERC721FVerify.hasTokens(FREEMINT_CONTRACT),
