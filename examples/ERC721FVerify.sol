f3a5808cc6de7da344c7d7022b03ec91ce2bfd4c
N1h1lum <zeronihilum@gmail.com>
2023-04-19 15:33:56 +0200

    Reworked imports to include package name

--- examples/ERC721FVerify.sol
+++ examples/ERC721FVerify.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9 <0.9.0;
-// import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "../contracts/token/ERC721/ERC721F.sol";
+import "@franknft.eth/erc721-f/contracts/token/ERC721/ERC721F.sol";
 
 interface WarmInterface {
     function balanceOf(
