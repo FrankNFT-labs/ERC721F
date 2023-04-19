f36b589f73c10c791a37525a7d762c681a3c4c23
N1h1lum <zeronihilum@gmail.com>
2023-04-19 15:00:54 +0200

    Rewrote contracts to allow delegatedMints and preSalePerms

--- examples/ERC721FVerify.sol
+++ examples/ERC721FVerify.sol
@@ -4,25 +4,24 @@ pragma solidity ^0.8.9 <0.9.0;
 import "../contracts/token/ERC721/ERC721F.sol";
 
 interface WarmInterface {
-    function ownerOf(
+    function balanceOf(
         address contractAddress,
-        uint256 tokenId
-    ) external view returns (address);
+        address owner
+    ) external view returns (uint256);
 }
 
 interface DelegateCashInterface {
-    function checkDelegateForToken(
+    function checkDelegateForContract(
         address delegate,
         address vault,
-        address contract_,
-        uint256 tokenId
+        address contract_
     ) external view returns (bool);
 }
 
 error ZeroAddressCheck();
 
 /**
- * @title ERC721FVerify - check for token ownership via contract, warm wallet and delegate cash
+ * @title ERC721FVerify
  * Based on https://etherscan.io/address/0xba5a9e9cbce12c70224446c24c111132becf9f1d#code
  * Warm Wallet https://github.com/wenewlabs/public/tree/main/HotWalletProxy
  * Delegate.cash https://github.com/delegatecash/delegation-registry
@@ -40,35 +39,22 @@ contract ERC721FVerify {
         DELEGATE_CASH_CONTRACT = _delegateCashContract;
     }
 
-    /**
-     * @notice verify contract token based claim using warm wallet and delegate cash
-     * @param tokenContract the smart contract address of the token
-     * @param tokenId the tokenId
-     */
-    function verifyTokenOwner(
+    function isValidClaimer(
         address tokenContract,
-        uint256 tokenId
+        address vault
     ) internal view returns (bool) {
-        address tokenOwner = ERC721F(tokenContract).ownerOf(tokenId);
+        return
+            DelegateCashInterface(DELEGATE_CASH_CONTRACT)
+                .checkDelegateForContract(msg.sender, vault, tokenContract);
+    }
 
-        // 1. check contract token owner
-        // 2. check warm wallet delegation - ownerOf()
-        //      all delegation
-        //      is a mapping of token owner's wallet to hot wallet
-        //      coldWalletToHotWallet[owner].walletAddress
-        // 3. check delegate.cash delegation - checkDelegateForToken()
-        //      checks three forms of delegation all, contract, and contract/token id
-        return (msg.sender == tokenOwner ||
-            msg.sender ==
-            WarmInterface(WARM_WALLET_CONTRACT).ownerOf(
+    function hasPreSalePermissions(
+        address tokenContract
+    ) internal view returns (bool) {
+        return
+            WarmInterface(WARM_WALLET_CONTRACT).balanceOf(
                 tokenContract,
-                tokenId
-            ) ||
-            DelegateCashInterface(DELEGATE_CASH_CONTRACT).checkDelegateForToken(
-                msg.sender,
-                tokenOwner,
-                tokenContract,
-                tokenId
-            ));
+                msg.sender
+            ) > 0;
     }
 }
--- examples/ERC721FVerifyImplementation.sol
+++ examples/ERC721FVerifyImplementation.sol
@@ -6,6 +6,12 @@ import "./ERC721FVerify.sol";
 
 contract ERC721FVerifyImplementation is ERC721FVerify, ERC721F {
     address public immutable FREEMINT_CONTRACT;
+    bool public saleIsActive;
+
+    modifier validMintRequest() {
+        require(msg.sender == tx.origin, "No contracts allowed");
+        _;
+    }
 
     constructor(
         address _warmContract,
@@ -18,14 +24,29 @@ contract ERC721FVerifyImplementation is ERC721FVerify, ERC721F {
         FREEMINT_CONTRACT = _freeMintContract;
     }
 
-    function claimToken(uint256 id) external {
-        _verifyValidClaimer(id);
-        _mint(msg.sender, id);
+    function flipSaleState() external onlyOwner {
+        saleIsActive = !saleIsActive;
     }
 
-    function _verifyValidClaimer(uint256 tokenId) private view {
-        if (!verifyTokenOwner((FREEMINT_CONTRACT), tokenId)) {
-            revert("Not a valid owner");
-        }
+    function singleMint(uint256 tokenId) external validMintRequest {
+        require(saleIsActive, "SALE is not active yet");
+        _mint(msg.sender, tokenId);
+    }
+
+    function singleMintOther(
+        uint256 tokenId,
+        address recipient
+    ) external validMintRequest {
+        require(saleIsActive, "SALE is not active yet");
+        require(
+            ERC721FVerify.isValidClaimer(address(this), recipient),
+            "Not delegated by recipient"
+        );
+        _mint(recipient, tokenId);
+    }
+
+    function preSaleMint(uint256 tokenId) external validMintRequest {
+        require(ERC721FVerify.hasPreSalePermissions(FREEMINT_CONTRACT));
+        _mint(msg.sender, tokenId);
     }
 }
