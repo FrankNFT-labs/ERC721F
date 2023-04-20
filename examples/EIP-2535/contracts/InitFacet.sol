18043063839060238c10536d0e42dcbf1ab4d94a
N1h1lum <zeronihilum@gmail.com>
2023-04-20 11:28:27 +0200

    Setting storage values and supportsInterface

---
+++ examples/EIP-2535/contracts/InitFacet.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9 <0.9.0;
+
+import {IDiamondLoupe} from "hardhat-deploy/solc_0.8/diamond/interfaces/IDiamondLoupe.sol";
+import {IERC173} from "hardhat-deploy/solc_0.8/diamond/interfaces/IERC173.sol";
+import {IERC165, IERC721, IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import {UsingDiamondOwner, IDiamondCut} from "hardhat-deploy/solc_0.8/diamond/UsingDiamondOwner.sol";
+import {ERC721FStorage} from "./ERC721F/ERC721FStorage.sol";
+import {WithStorage} from "./WithStorage.sol";
+
+contract InitFacet is UsingDiamondOwner, WithStorage {
+    function f() internal pure returns (ERC721FStorage.Layout storage) {
+        return ERC721FStorage.layout();
+    }
+
+    function init() external onlyOwner {
+        if (s().isInitialized) return;
+
+        f()._name = "FreeMint";
+        f()._symbol = "FM";
+
+        s().MAX_TOKENS = 10000;
+        s().MAX_PURCHASE = 31;
+
+        ds().supportedInterfaces[type(IERC165).interfaceId] = true;
+        ds().supportedInterfaces[type(IDiamondCut).interfaceId] = true;
+        ds().supportedInterfaces[type(IDiamondLoupe).interfaceId] = true;
+        ds().supportedInterfaces[type(IERC173).interfaceId] = true;
+        ds().supportedInterfaces[type(IERC721).interfaceId] = true;
+        ds().supportedInterfaces[type(IERC721Metadata).interfaceId] = true;
+
+        s().isInitialized = true;
+    }
+}
