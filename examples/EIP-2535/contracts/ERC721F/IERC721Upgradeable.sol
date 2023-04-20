5fc15a98de64a7a501f1cc3eba3591b8bf7fa29a
N1h1lum <zeronihilum@gmail.com>
2023-04-20 09:38:03 +0200

    Corrected spelling mistake in name

--- examples/EIP-2535/contracts/ERC721F/IERC721Upgradeable.sol
+++ examples/EIP-2535/contracts/ERC721F/IERC721Upgradeable.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.0;
 /**
  * @dev Required interface of an ERC721 compliant contract.
  */
-interface IERC721Upgradable {
+interface IERC721Upgradeable {
     /**
      * @dev Emitted when `tokenId` token is transferred from `from` to `to`.
      */
