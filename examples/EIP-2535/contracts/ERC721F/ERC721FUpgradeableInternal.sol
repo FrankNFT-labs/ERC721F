af27ea6d8f1b9a1e8a218c42d580ca61a10e8d60
N1h1lum <zeronihilum@gmail.com>
2023-04-20 10:50:08 +0200

    Imported Address utils to replace isContract function

--- examples/EIP-2535/contracts/ERC721F/ERC721FUpgradeableInternal.sol
+++ examples/EIP-2535/contracts/ERC721F/ERC721FUpgradeableInternal.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.9 <0.9.0;
 
 import {ERC721FStorage} from "./ERC721FStorage.sol";
 import {IERC721Upgradeable} from "./IERC721Upgradeable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 /**
  * @title ERC721 token receiver interface
@@ -36,6 +37,7 @@ interface IERC721ReceiverUpgradeable {
  */
 contract ERC721FUpgradeableInternal is IERC721Upgradeable {
     using ERC721FStorage for ERC721FStorage.Layout;
+    using Address for address;
 
     // ERC721
 
@@ -328,7 +330,7 @@ contract ERC721FUpgradeableInternal is IERC721Upgradeable {
         uint256 tokenId,
         bytes memory data
     ) internal returns (bool) {
-        if (_isContract(to)) {
+        if (to.isContract()) {
             try
                 IERC721ReceiverUpgradeable(to).onERC721Received(
                     _msgSender(),
@@ -416,44 +418,6 @@ contract ERC721FUpgradeableInternal is IERC721Upgradeable {
 
     function _msgSender() internal view virtual returns (address) {
         return msg.sender;
-    }
-
-    /**
-     * @dev Returns true if `account` is a contract.
-     *
-     * [IMPORTANT]
-     * ====
-     * It is unsafe to assume that an address for which this function returns
-     * false is an externally-owned account (EOA) and not a contract.
-     *
-     * Among others, `isContract` will return false for the following
-     * types of addresses:
-     *
-     *  - an externally-owned account
-     *  - a contract in construction
-     *  - an address where a contract will be created
-     *  - an address where a contract lived, but was destroyed
-     *
-     * Furthermore, `isContract` will also return true if the target contract within
-     * the same transaction is already scheduled for destruction by `SELFDESTRUCT`,
-     * which only has an effect at the end of a transaction.
-     * ====
-     *
-     * [IMPORTANT]
-     * ====
-     * You shouldn't rely on `isContract` to protect against flash loan attacks!
-     *
-     * Preventing calls from contracts is highly discouraged. It breaks composability, breaks support for smart wallets
-     * like Gnosis Safe, and does not provide security since it can be circumvented by calling from a contract
-     * constructor.
-     * ====
-     */
-    function _isContract(address account) internal view returns (bool) {
-        // This method relies on extcodesize/address.code.length, which returns 0
-        // for contracts in construction, since the code is only stored at the end
-        // of the constructor execution.
-
-        return account.code.length > 0;
     }
 
     // ERC721F
