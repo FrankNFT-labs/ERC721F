637c482db50be6b37206f913435ed697e93e15cf
N1h1lum <zeronihilum@gmail.com>
2023-04-21 15:28:55 +0200

    Removed disfunctional test and made it passthrough to EIP-2535 workspace

--- test/hardhat/examples/EIP-2535.test.js
+++ test/hardhat/examples/EIP-2535.test.js
@@ -1,22 +1,10 @@
-const { deployments } = require("hardhat");
+const { execSync } = require("child_process");
 
-const setupTest = deployments.createFixture(
-  async ({ deployments, getNamedAccounts, ethers }, options) => {
-    await deployments.fixture(); // ensure you start from a fresh deployments
-    const { tokenOwner } = await getNamedAccounts();
-    const TokenContract = await ethers.getContract("Token", tokenOwner);
-    await TokenContract.mint(10).then((tx) => tx.wait()); //this mint is executed once and then `createFixture` will ensure it is snapshotted
-    return {
-      tokenOwner: {
-        address: tokenOwner,
-        TokenContract,
-      },
-    };
-  }
-);
-describe("Token", () => {
-  it("testing 1 2 3", async function () {
-    const { tokenOwner } = await setupTest();
-    await tokenOwner.TokenContract.mint(2);
+describe("Passing command to EIP-2535 workspace", () => {
+  it("should run npm test in eip-2535 workspace", () => {
+    const workspace = "eip-2535";
+    const cmd = `npm run test --workspace=${workspace}`;
+    const output = execSync(cmd);
+    console.log(output.toString());
   });
 });
