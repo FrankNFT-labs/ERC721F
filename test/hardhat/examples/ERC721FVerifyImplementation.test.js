97c878fa8fccfda98c3da87dd31777b311ff3804
N1h1lum <zeronihilum@gmail.com>
2023-07-04 16:58:04 +0200

    Updated test file to take ERC721FVerifyImplementation changes into account

--- test/hardhat/examples/ERC721FVerifyImplementation.test.js
+++ test/hardhat/examples/ERC721FVerifyImplementation.test.js
@@ -23,42 +23,54 @@ describe("ERC721FVerifyImplementation", function () {
       freeMint.address
     );
 
+    await freeMint.flipSaleState();
+    await freeMint.mint(1);
+
     await hotWalletProxy.setHotWallet(addr1.address, 9999999999, false);
-    await delegationRegistry.delegateForContract(
+    await delegationRegistry.delegateForToken(
       addr2.address,
-      token.address,
+      freeMint.address,
+      0,
       true
     );
-    await freeMint.flipSaleState();
-    await freeMint.mint(5);
-    await freeMint.flipSaleState();
 
-    return { freeMint, token, owner, addr1, addr2, addr3, hotWalletProxy };
+    return {
+      freeMint,
+      token,
+      owner,
+      addr1,
+      addr2,
+      addr3,
+      hotWalletProxy,
+      delegationRegistry,
+    };
   }
 
   describe("mint", function () {
     it("Doesn't allow mints when address does not have any tokens in freeMint or isn't delegated by vault", async function () {
-      const { token, owner, addr3 } = await loadFixture(deployTokenFixture);
+      const { token, addr3 } = await loadFixture(deployTokenFixture);
 
-      await expect(
-        token.connect(addr3).mint(1, owner.address)
-      ).to.be.rejectedWith(
-        "Must have tokens in FreeMint or be delegated by `vault`"
+      await expect(token.connect(addr3).mint(0)).to.be.rejectedWith(
+        "Must have tokens in FreeMint"
       );
     });
 
-    it("Allows minting when owning tokens in FreeMint", async function () {
-      const { token, owner, addr1 } = await loadFixture(deployTokenFixture);
+    it("Allows minting when owner of token", async function () {
+      const { token } = await loadFixture(deployTokenFixture);
 
-      await expect(token.connect(addr1).mint(1, owner.address)).to.not.be
-        .rejected;
+      await expect(token.mint(0)).to.not.be.rejected;
+    });
+
+    it("Allows minting when owning tokens in FreeMint", async function () {
+      const { token, addr1 } = await loadFixture(deployTokenFixture);
+
+      await expect(token.connect(addr1).mint(0)).to.not.be.rejected;
     });
 
     it("Allows minting when delegated", async function () {
-      const { token, owner, addr2 } = await loadFixture(deployTokenFixture);
+      const { token, addr2 } = await loadFixture(deployTokenFixture);
 
-      await expect(token.connect(addr2).mint(1, owner.address)).to.not.be
-        .rejected;
+      await expect(token.connect(addr2).mint(0)).to.not.be.rejected;
     });
   });
 });
