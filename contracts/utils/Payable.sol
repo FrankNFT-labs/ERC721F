c1cc7cbdbdcf7d897f29b987b9330d25cfed0cfe
FrankNFT.eth <frank.poncelet@gmail.com>
2023-07-13 12:58:19 +0200

    Update Payable.sol
    added Contract description

--- contracts/utils/Payable.sol
+++ contracts/utils/Payable.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9 <0.9.0;
 
+/**
+ * @title Payable
+ * @notice This abstract contract provides a simple and secure way to handle Ether payments and withdrawals.
+ * It allows the contract to receive Ether and provides an internal function for derived contracts to withdraw Ether.
+ */
 abstract contract Payable {
     /**
     * Helper method to allow ETH withdraws.
