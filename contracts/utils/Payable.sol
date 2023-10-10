0e5d726cf7b060e0ce9fef5c3c7386901bc7ff81
N1h1lum <zeronihilum@gmail.com>
2023-10-10 11:38:24 +0200

    Disabled function ordering for fallback payable

--- contracts/utils/Payable.sol
+++ contracts/utils/Payable.sol
@@ -8,13 +8,14 @@ pragma solidity ^0.8.9 <0.9.0;
  */
 abstract contract Payable {
     /**
-    * Helper method to allow ETH withdraws.
-    */
+     * Helper method to allow ETH withdraws.
+     */
     function _withdraw(address _address, uint256 _amount) internal {
-        (bool success, ) = _address.call{ value: _amount }("");
+        (bool success, ) = _address.call{value: _amount}("");
         require(success, "Failed to withdraw Ether");
     }
 
     // contract can recieve Ether
-    receive() external payable { }
+    // solhint-disable-next-line ordering
+    receive() external payable {}
 }
