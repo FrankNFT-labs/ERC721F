830d7ff6ed8a104fbff83e3d89c79170c799d220
N1h1lum <zeronihilum@gmail.com>
2023-10-11 09:50:21 +0200

    Add override to _update

--- contracts/token/ERC721/ERC721F.sol
+++ contracts/token/ERC721/ERC721F.sol
@@ -85,7 +85,7 @@ contract ERC721F is Ownable, ERC721 {
         address to,
         uint256 tokenId,
         address auth
-    ) internal virtual returns (address) {
+    ) internal virtual override returns (address) {
         address from = super._update(to, tokenId, auth);
         if (from == address(0)) {
             _tokenSupply++;
