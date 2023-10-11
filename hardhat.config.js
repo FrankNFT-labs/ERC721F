ea9c73ef9560102e49f06f7c21cc24c5a5756f89
N1h1lum <zeronihilum@gmail.com>
2023-10-11 09:03:45 +0200

    Bump solidity version

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("hardhat-gas-reporter");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: "0.8.13",
+    version: "0.8.20",
     settings: {
       optimizer: {
         enabled: true,
