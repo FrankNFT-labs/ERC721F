3796d2cb65106026fdbc33db986b2f9c9c9db8fe
N1h1lum <zeronihilum@gmail.com>
2023-10-16 09:22:22 +0200

    Removed filename extraction

--- scripts/sh/verify-contract.sh
+++ scripts/sh/verify-contract.sh
@@ -6,8 +6,6 @@ if [[ $filter != *.sol ]]; then
     echo "Invalid filepath. It must end with '.sol'."
     exit 1
 else
-    filename=$(basename "$filter" .sol)
-
     WHITELIST_PATH=$filter npx hardhat compile
 fi
 
