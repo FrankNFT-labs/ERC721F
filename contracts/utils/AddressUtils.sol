bd18258b7e1f59bc9e32a0f73676a977e77070af
Frank <frank.poncelet@hyper-labs.digital>
2024-07-05 13:44:16 +0200

    added AdressUtils.sol

---
+++ contracts/utils/AddressUtils.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title AddressUtils
+ * @dev A collection of utility functions related to the address type.
+ */
+library AddressUtils {
+    /**
+     * @notice Checks if the provided address is a contract.
+     * @dev This function checks the size of the code at the given address.
+     * @param _address The address to check.
+     * @return bool Returns true if the address is a contract, false otherwise.
+     */
+    function _isContract(address _address) internal view returns (bool) {
+        uint32 size;
+        assembly {
+            size := extcodesize(_address)
+        }
+        return (size > 0);
+    }
+}
