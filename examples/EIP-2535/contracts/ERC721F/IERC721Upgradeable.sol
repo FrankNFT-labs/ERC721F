c83f746fbbe2591d4dd93a02e966d1694ff524de
FrankNFT <frank.poncelet@gmail.com>
2026-03-24 09:33:00 +0100

    align eip-2535 erc721 interface pragma with project compiler

--- examples/EIP-2535/contracts/ERC721F/IERC721Upgradeable.sol
+++ examples/EIP-2535/contracts/ERC721F/IERC721Upgradeable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v4.8.0) (token/ERC721/IERC721.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20 <0.9.0;
 
 /**
  * @dev Required interface of an ERC721 compliant contract.
